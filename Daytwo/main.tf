6b3c76a6714c0401d25a883170d250797d19109d
subbu614 <subramanyamsai614@gmail.com>
2025-05-21 08:39:04 +0530

    day 1, day 2

--- Daytwo/main.tf
+++ Daytwo/main.tf
@@ -1,1 +1,9 @@
+resource "aws_instance" "name" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    key_name = var.key
+    tags = {
+        Name = "day-1"
+    }
+}
 
---
+++ Daytwo/terraform.tfvars
@@ -0,0 +1,5 @@
+ami_id = "ami-0038df39db13a87e2"
+instance_type = "t2.micro"
+key = "Ashok"
+
+
